9aaa5d470cf433d92317e0df5017852d550dce0a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXGrid - How to embed the ChartControl into the GridControl cell
+
+
+<p>It's necessary to implement the CellTemplate with the ChartControl, and bind any ChartControl property to a cell value, for example. <br />
+In the sample, the first SeriesPoint in the Points collection is bound to a value in the cell</p>
+
+<br/>
+
+
---
+++ VB/UnboundCheckColumn/Window1.xaml.vb
@@ -0,0 +1,63 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+
+Namespace UnboundCheckColumn
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Private list As List(Of TestData)
+
+		Public Sub New()
+			InitializeComponent()
+			list = New List(Of TestData)()
+			For i As Integer = 0 To 2
+				list.Add(New TestData() With {.Id = Guid.NewGuid(), .Number = i})
+			Next i
+			grid.ItemsSource = list
+		End Sub
+
+		Private Sub grid_CustomUnboundColumnData(ByVal sender As Object, ByVal e As GridColumnDataEventArgs)
+			If e.Column.FieldName = "Chart" Then
+				Dim key As Double = CInt(Fix(e.GetListSourceFieldValue("Number")))
+				If e.IsGetData Then
+					e.Value = CDbl(key)
+				End If
+			End If
+		End Sub
+	End Class
+	Public Class TestData
+		Private privateId As Guid
+		Public Property Id() As Guid
+			Get
+				Return privateId
+			End Get
+			Set(ByVal value As Guid)
+				privateId = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+	End Class
+End Namespace
